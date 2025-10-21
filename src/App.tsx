cc280008a5973df57852820785885a0ca0fbc987
asseternity <nakupovasset@gmail.com>
2025-10-21 19:32:32 +0400

    feat: init project, add shadcn

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { useState } from 'react';
+import { Gallery6 } from './components/Gallery6';
+
+function App() {
+  return (
+    <div>
+      <Gallery6 />
+    </div>
+  );
+}
+
+export default App;
