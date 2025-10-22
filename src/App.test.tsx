4595ed68c67ce686dea80211b628020506909506
asseternity <nakupovasset@gmail.com>
2025-10-22 23:07:36 +0400

    feat: init testing

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,23 +2,29 @@ import { screen, render } from '@testing-library/react';
 import App from './App';
 import { vi } from 'vitest';
 
+function sum(x: number, y: number): number {
+  return x + y;
+}
+
 vi.mock('Gallery6', () => ({
   default: () => {
     return <div>{'Mock'}</div>;
   },
 }));
 
-describe('App tests', () => {
+describe('Testing compilation', () => {
   it('compile test', () => {
     expect(1).toBe(1);
   });
-  it('should render the title', () => {
-    render(<App />);
+  it('adds two numbers', () => {
+    const result = sum(1, 2);
+    expect(result).toBe(3);
+  });
+});
 
-    expect(
-      screen.getByRole('heading', {
-        level: 1,
-      })
-    ).toHaveTextContent('Gallery');
+describe('App tests', () => {
+  it('renders the heading', () => {
+    render(<App />);
+    expect(screen.getByRole('heading')).toBeInTheDocument();
   });
 });
